f56a10fb2468a2a70a429d98dcc999782b1f6e35
murzFox <murzFox@protonmail.com>
2021-03-03 21:49:03 +0200

    start coding
    start of the project

---
+++ src/components/app/root/view.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RoutesSwitch } from 'routes';
+
+const View = React.memo(() => {
+	return (
+		<RoutesSwitch />
+	);
+});
+
+export { View };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider as ReduxProvider } from 'react-redux';
+import { ConnectedRouter as ConnectedRouterProvider } from 'connected-react-router';
+import { Root } from 'components';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = (Component, root, done = () => {}) => {
+	ReactDOM.render(
+		<ReduxProvider store={store}>
+			<ConnectedRouterProvider history={history}>
+				<Component />
+			</ConnectedRouterProvider>
+		</ReduxProvider>,
+		root,
+		done,
+	);
+};
+
+render(Root, root);
